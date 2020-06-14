76f54bb29e1f44096c245d29139ebbd6e38dd5e6
Ryan Mehri <ryan.mehri1@gmail.com>
2020-06-14 13:46:46 -0600

    Add rule and stepping

--- src/Life.hs
+++ src/Life.hs
@@ -56,3 +56,32 @@ data Cell = Alive | Dead
 mkGrid :: [Coord] -> Grid Cell
 mkGrid xs = store lookup (0, 0)
     where lookup crd = if crd `elem` xs then Alive else Dead
+
+-- | Generic rules for the game that determine how a cell changes based on the current grid.
+type Rule = Grid Cell -> Cell
+
+-- | The 8 neighbouring tiles relative to (0, 0), the cell itself
+neighbourCoords :: [Coord]
+neighbourCoords =
+    [ (x, y) | x <- [-1, 0, 1], y <- [-1, 0, 1], (x, y) /= (0, 0) ]
+
+-- | The typical rules for the Game of Life.
+-- https://en.wikipedia.org/wiki/Conway's_Game_of_Life
+-- From wikipedia, at each step, the following transitions occur:
+-- * Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+-- * Any live cell with two or three live neighbours lives on to the next generation.
+-- * Any live cell with more than three live neighbours dies, as if by overpopulation.
+-- * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+basicRule :: Rule
+basicRule g = case cellStatus of
+    Alive -> if numNeighboursAlive `elem` [2, 3] then Alive else Dead
+    Dead  -> if numNeighboursAlive == 3 then Alive else Dead
+  where
+    cellStatus = extract g
+    addCoords (x, y) (x', y') = (x + x', y + y')
+    neighbours         = experiment (\s -> addCoords s <$> neighbourCoords) g
+    numNeighboursAlive = length (filter (== Alive) neighbours)
+
+-- | Advances the grid by one step according to the given rule set.
+step :: Rule -> Grid Cell -> Grid Cell
+step = extend
