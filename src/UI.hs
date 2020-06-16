00285a55276242cfa001f52adb048bfec0fad859
Ryan Mehri <ryan.mehri1@gmail.com>
2020-06-16 17:45:42 -0600

    Add ability to stop and start simulation

--- src/UI.hs
+++ src/UI.hs
@@ -7,17 +7,41 @@ import           Control.Comonad.Representable.Store
                                                 , StoreT(..)
                                                 , runStore
                                                 )
+import           Control.Comonad                ( extract )
 import           Graphics.Gloss
+import           Graphics.Gloss.Interface.IO.Interact
+
+-- | Uses a rule and grid to continuously simulate the game.
+-- Steps at each iteration as long as the game is not paused.
+runSimulation :: Rule -> Grid Cell -> IO ()
+runSimulation r g = play window
+                         white
+                         2
+                         (g, False)
+                         (centerGrid . renderGrid . fst)
+                         handleEvent
+                         (\_ (g', b) -> (if b then step r g' else g', b))
+
+-- | Window as big as the picture of the grid is.
+window :: Display
+window = InWindow "Game of Life" (sideLen, sideLen) (0, 0)
+  where sideLen = gridSize * round squareLen
+
+-- | Side length of a single square in the picture.
+squareLen :: Float
+squareLen = 20
 
 -- | Render a picture of the grid at a given instance.
 renderGrid :: Grid Cell -> Picture
 renderGrid g = pictures
   [ translate (fromIntegral x * squareLen)
               (-(fromIntegral y * squareLen))
-              (color c (rectangleSolid squareLen squareLen))
+              singleSquare
   | x <- [0 .. gridSize - 1]
   , y <- [0 .. gridSize - 1]
   , let c = chooseColor ((fst $ runStore g) (y, x))
+  , let singleSquare = rectangleWire squareLen squareLen
+          <> color c (rectangleSolid squareLen squareLen)
   ]
  where
   chooseColor Alive = makeColorI 254 95 85 255
@@ -29,21 +53,15 @@ centerGrid = translate (-distToMiddle) distToMiddle
  where
   distToMiddle = fromIntegral (gridSize `div` 2) * squareLen - (squareLen / 2)
 
--- | Side length of a single square in the picture.
-squareLen :: Float
-squareLen = 20
+-- | Handles adding and removing cells as well as pausing the game.
+-- Click on a cell to change it from Dead to Alive and vice versa
+-- Press the space key to start and stop the simulation.
+handleEvent :: Event -> (Grid Cell, Bool) -> (Grid Cell, Bool)
+handleEvent e w@(g, b) = case e of
+  (EventKey (SpecialKey KeySpace) Down _ _) -> (g, not b)
+  (EventKey (MouseButton LeftButton) Down _ (x, y)) -> undefined
+  _ -> w
 
--- | Window as big as the picture of the grid is.
-window :: Display
-window = InWindow "Game of Life" (sideLen, sideLen) (0, 0)
-  where sideLen = gridSize * round squareLen
-
--- | Uses a rule and grid to continuously simulate the game by stepping at each iteration.
-runSimulation :: Rule -> Grid Cell -> IO ()
-runSimulation r g =
-  simulate window white 2 g (centerGrid . renderGrid) (\_ _ -> step r)
-
--- TODO: add grid to distinguish squares better
 -- TODO: add cycle counter
--- TODO: mouse interaction to set up initial grid and start key to run
+-- TODO: mouse interaction to set up initial grid 
 -- TODO: add variable speed or maybe manually control stepping
